2a126855e6faef8f528935e78b1fd4bee79a9c0e
2chaelim <charry8540@gmail.com>
2023-02-24 15:01:27 +0900

    2/24

---
+++ prj1.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".reviewSwiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+const $gnb = document.querySelector(".gnb");
+const $ham = document.querySelector(".bar1");
+$ham.addEventListener("click", () => {
+  $gnb.classList.toggle("on");
+});
